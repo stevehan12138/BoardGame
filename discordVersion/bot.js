7866ee70042ef2f9c32ea4ec669789be7cf4ffce
stevehan12138 <stevehan12138@gmail.com>
2020-04-22 21:27:30 -0400

    work

--- discordVersion/bot.js
+++ discordVersion/bot.js
@@ -1,1 +1,10 @@
-require('discord.js')
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login('NzAyNjgwNDg0OTU3NjUxMDE3.XqDuiA.inCIPKrPq9x6kb3vv7zbFH3EJxQ');
